213bbdbf8fc47ccb81b01305ac6b38e5d2c86f57
Richard Janis Beckert <janis.beckert@gmail.com>
2015-02-06 18:52:36 +0100

    Add RDQ decomposition, streamline QDR.

--- linalg.f90
+++ linalg.f90
@@ -1,11 +1,13 @@
 module linalg
     use types
     use matrix_routines
+    use utils
     implicit none
 
-    public :: qdr
     private
-
+    public :: qdr, rdq
+    public :: stable_mm_qdr, stable_mm_rdq
+    
     contains
         subroutine qdr(a, q, d, r, info) !{{{
             real(dp), dimension(:,:), intent(in) :: a
@@ -17,27 +19,28 @@ module linalg
 
             integer, dimension(:), allocatable :: sh
 
-            integer :: lda, lwork, k, m, n, mn, ws
+            integer :: lwork, m, ws
             integer :: i,s
 
             q = a
 
             sh = shape(a)
             m = sh(1)
-            n = sh(2)
 
-            lda = m
+            if (m /= sh(2)) then
+                call stop_error("qdr: non-square matrix detected.")
+            end if
 
-            mn = min(m,n)
-            allocate(tau(mn))
+            allocate(tau(m))
             allocate(work_q(1))
+
             ! Compute the optimal work array: query
-            call dgeqrf(m, n, q, lda, tau, work_q, -1, info)
+            call dgeqrf(m, m, q, m, tau, work_q, -1, info)
             ws = int(abs(work_q(1)))
             allocate(work(ws))
 
             ! Compute the QR factorization
-            call dgeqrf(m, n, q, lda, tau, work, ws, info)
+            call dgeqrf(m, m, q, m, tau, work, ws, info)
 
             ! R = D R'
             tr = triangular_upper(q)
@@ -50,23 +53,25 @@ module linalg
             end do
 
             r = tr
-            call dgemm('n','n',mn,n,mn,1.0_dp,diagonal(vdi),mn,tr,mn,0.0_dp,r,mn)
+            call dgemm('n','n',m,m,m,1.0_dp,diagonal(vdi),m,tr,m,0.0_dp,r,m)
 
             d = diagonal(vd)
 
             ! Compute the orthogonal matrix from the reflectors resulting from dgeqrf
-            k = size(tau)
-            call dorgqr(m, n, k, q, lda, tau, work_q, -1, info)
+            call dorgqr(m, m, m, q, m, tau, work_q, -1, info)
+
+            ! Reallocate the work array if the optimal size is different from the QR factorization above.
             if (work_q(1) .ne. ws) then
                 ws = work_q(1)
                 deallocate(work)
                 allocate(work(ws))
             end if
 
-            call dorgqr(m, n, k, q, lda, tau, work, ws, info)
+            call dorgqr(m, m, m, q, m, tau, work, ws, info)
         end subroutine qdr !}}}
 
         subroutine rdq(a, r, d, q, info) !{{{
+            ! RDQ decomposition of the matrix A. Assumes that A is quadratic!
             real(dp), dimension(:,:), intent(in) :: a
             real(dp), dimension(:,:), allocatable, intent(out) :: r, d, q
             integer, intent(out) :: info
@@ -76,7 +81,7 @@ module linalg
 
             integer, dimension(:), allocatable :: sh
 
-            integer :: lda, lwork, k, m, n, mn, ws
+            integer :: lwork, m, n, mn, ws
             integer :: i,s
 
             q = a
@@ -84,21 +89,20 @@ module linalg
             sh = shape(a)
             m = sh(1)
             n = sh(2)
+            if (m /= n) then
+                call stop_error("rdq: non-square matrix detected.")
+            end if
 
-            lda = m
-
-            mn = min(m,n)
-            allocate(tau(mn))
+            allocate(tau(m))
             allocate(work_q(1))
             ! Compute the optimal work array: query
-            call dgerqf(m, n, q, lda, tau, work_q, -1, info)
+            call dgerqf(m, m, q, m, tau, work_q, -1, info)
             ws = int(abs(work_q(1)))
             allocate(work(ws))
 
-            ! Compute the QR factorization
-            call dgeqrf(m, n, q, lda, tau, work, ws, info)
+            ! Compute the RQ factorization
+            call dgerqf(m, m, q, m, tau, work, ws, info)
 
-            ! R = D R'
             tr = triangular_upper(q)
 
             vd = diagonal(tr)
@@ -109,20 +113,21 @@ module linalg
             end do
 
             r = tr
-            call dgemm('n','n',mn,n,mn,1.0_dp,diagonal(vdi),mn,tr,mn,0.0_dp,r,mn)
+            call dgemm('n','n',m,m,m,1.0_dp,tr,m,diagonal(vdi),m,0.0_dp,r,m)
 
             d = diagonal(vd)
 
-            ! Compute the orthogonal matrix from the reflectors resulting from dgeqrf
-            k = size(tau)
-            call dorgqr(m, n, k, q, lda, tau, work_q, -1, info)
+            ! Compute the orthogonal matrix from the reflectors resulting from dgerqf
+            call dorgrq(m, m, m, q, m, tau, work_q, -1, info)
+
+            ! Reallocate the work array if the optimal size is different from the RQ factorization above.
             if (work_q(1) .ne. ws) then
                 ws = work_q(1)
                 deallocate(work)
                 allocate(work(ws))
             end if
 
-            call dorgqr(m, n, k, q, lda, tau, work, ws, info)
+            call dorgrq(m, m, m, q, m, tau, work, ws, info)
         end subroutine rdq !}}}
 
 end module linalg
